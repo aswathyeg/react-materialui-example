c6e951d096e862509f56d3f2b2a47042d601755c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-23 22:06:32 +0530

    added toolbar and typography

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import { Tabs, Tab, Toolbar, Button } from "@mui/material";
+
+const Navbar = () => {
+    return (
+        <div>
+
+            <AppBar>
+                <Toolbar>
+                <Typography> bodhii </Typography>
+
+                <Tabs>
+                    <Tab label='Courses'/>
+                </Tabs>
+                <Tabs>
+                    <Tab label='Fees'/>
+                </Tabs>
+                <Tabs>
+                    <Tab label='Parents'/>
+                </Tabs><Tabs>
+                    <Tab label='Holidays'/>
+                </Tabs><Tabs>
+                    <Tab label='Teachers'/>
+                </Tabs><Tabs>
+                    <Tab label='Openings'/>
+                </Tabs>
+                <Button varient="contained" color="secondary">Profile</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+};
+export default Navbar;
