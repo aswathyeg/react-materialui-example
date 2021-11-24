d3923b4d82fd76d370476b9140f40447dedf9e8d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-25 01:05:46 +0530

    drawer component

---
+++ src/components/Navbar/DrawerComponent/DrawerComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List'
+ const DrawerComponent =()=>{
+     return(
+         <Drawer>
+             <List>
+
+
+             </List>
+
+         </Drawer>
+     )
+ }
+ export default DrawerComponent;
