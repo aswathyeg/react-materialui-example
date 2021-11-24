7b030aa0c407e4e66c181a994d4927d6b5f88917
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-24 17:16:03 +0530

    added primary and secondary colors

--- src/components/utils/theme.js
+++ src/components/utils/theme.js
@@ -2,7 +2,11 @@ import { createTheme } from '@mui/material/styles'
 const theme=createTheme({
 palette:{
     primary:{
-        main:'yellow'
+        main:'#26a69a',
+    },
+    secondary:{
+        main:'#c51162',
     }
 }
-})
+});
+export default theme;
