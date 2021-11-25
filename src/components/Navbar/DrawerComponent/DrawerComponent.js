e7626eef0f4c00927e9c2a7478df10ee2777cb18
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-25 23:28:18 +0530

    added drawer

--- src/components/Navbar/DrawerComponent/DrawerComponent.js
+++ src/components/Navbar/DrawerComponent/DrawerComponent.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List'
+import { ListItem, ListItemText } from '@mui/material';
  const DrawerComponent =()=>{
      return(
          <Drawer>
              <List>
+<ListItem>
+<ListItemText>
+    Classes
+</ListItemText>
 
+</ListItem>
 
              </List>
 
