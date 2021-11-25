cb91f66a1b38135e1c3710ef832745734318e6f3
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-25 23:28:33 +0530

    updated

--- src/components/utils/theme.js
+++ src/components/utils/theme.js
@@ -1,12 +1,18 @@
 import { createTheme } from '@mui/material/styles'
 const theme=createTheme({
+  
 palette:{
+   
     primary:{
         main:'#26a69a',
     },
     secondary:{
         main:'#c51162',
-    }
-}
+    },
+    
+},
+// mode:'dark',//dark theme
+
 });
+
 export default theme;
