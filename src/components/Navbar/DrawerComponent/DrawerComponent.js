d491c931136957e106bfef1b6b73d604d4ab56bd
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-26 02:40:17 +0530

    added margin to drawer

--- src/components/Navbar/DrawerComponent/DrawerComponent.js
+++ src/components/Navbar/DrawerComponent/DrawerComponent.js
@@ -1,59 +1,73 @@
 import React, { useState } from 'react';
 import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List'
-import { ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import List from '@mui/material/List';
+import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { createStyles, makeStyles } from '@mui/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const useStyles = makeStyles(theme => ({
+    menuIconContainer: {
+        marginLeft: 'auto',
+    },
+
+}));
+
 const DrawerComponent = () => {
     const [openDrawer, setOpenDrawer] = useState(true);
+    const classes = useStyles();
+
     return (
         <>
-        <Drawer anchor='right' onClose={() => {
-            setOpenDrawer(false)
-        }} open={openDrawer}>
-            <List>
-                <ListItem divider button> 
-                    <ListItemIcon>
-                        <ListItemText>
-                            Classes
-                        </ListItemText>
-                    </ListItemIcon>
+            <Drawer anchor='right' onClose={() => {
+                setOpenDrawer(false)
+            }} open={openDrawer}>
+
+                <List>
+                    <ListItem divider button>
+                        <ListItemIcon>
+                            <ListItemText>
+                                Classes
+                            </ListItemText>
+                        </ListItemIcon>
                     </ListItem>
 
 
-                   <ListItem divider button> 
-                    <ListItemIcon>
-                        <ListItemText>
-                            Teachers
-                        </ListItemText>
-                    </ListItemIcon>
+                    <ListItem divider button>
+                        <ListItemIcon>
+                            <ListItemText>
+                                Teachers
+                            </ListItemText>
+                        </ListItemIcon>
                     </ListItem>
-                    <ListItem divider button> 
-                    <ListItemIcon>
-                        <ListItemText>
-                            Parents
-                        </ListItemText>
-                    </ListItemIcon>
+                    <ListItem divider button>
+                        <ListItemIcon>
+                            <ListItemText>
+                                Parents
+                            </ListItemText>
+                        </ListItemIcon>
                     </ListItem>
-                    <ListItem divider button> 
-                    <ListItemIcon>
-                        <ListItemText>
-                            Fees
-                        </ListItemText>
-                    </ListItemIcon>
+                    <ListItem divider button>
+                        <ListItemIcon>
+                            <ListItemText>
+                                Fees
+                            </ListItemText>
+                        </ListItemIcon>
                     </ListItem>
-                    <ListItem divider button> 
-                    <ListItemIcon>
-                        <ListItemText>
-                            Holidays
-                        </ListItemText>
-                    </ListItemIcon>
+                    <ListItem divider button>
+                        <ListItemIcon>
+                            <ListItemText>
+                                Holidays
+                            </ListItemText>
+                        </ListItemIcon>
                     </ListItem>
+                </List>
 
-
-                
-
-            </List>
-
-        </Drawer>
+            </Drawer>
+            <IconButton className={classes.menuIconContainer} onClick={() => {
+                setOpenDrawer(!openDrawer)
+            }}>
+                <MenuIcon />
+            </IconButton>
         </>
     )
 }
